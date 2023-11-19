b9b4284f32f64b14ab7d4387372de956503c2687
Jonas Barros <Jonas_barros@outlook.com>
2023-11-18 22:42:58 -0300

    primeiro commit

---
+++ src/test/java/com/br/fiap/videostream/VideostreamApplicationTests.java
@@ -0,0 +1,16 @@
+package com.br.fiap.videostream;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideostreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+
+	}
+
+}
